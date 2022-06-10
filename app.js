37e0f3d4e73894f34a0e1a6392f536c2f70f62a8
kblacGH <kblackiam@gmail.com>
2022-06-09 21:47:59 -0400

    MoveThing

--- app.js
+++ app.js
@@ -22,13 +22,14 @@ app.set('view engine', 'hbs');
 
 
 //Connection Pool
-const pool = mysql.createPool({
+var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b2a24ba299581b',
     password: '671c84fb',
     database: 'heroku_282c9d407c3e7e2'
 });
+module.exports = connection;
 
 pool.getConnection((err, connection)=> {
     if(err) throw err; //not connected
